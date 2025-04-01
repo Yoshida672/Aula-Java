abd7e4fdb741c838e124dffdc7ded5f05bf6ea27
Yoshida672 <ericissamuyoshida@gmail.com>
2025-04-01 12:06:13 -0300

    Aula01-04

---
+++ src/main/java/br/com/fiap/apisecurity/controller/UserController.java
@@ -0,0 +1,29 @@
+package br.com.fiap.apisecurity.controller;
+
+import br.com.fiap.apisecurity.entity.User;
+import br.com.fiap.apisecurity.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository){
+        this.userRepository=userRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        List<User> users=userRepository.findAll();
+        return ResponseEntity.ok(users);
+    }
+}
